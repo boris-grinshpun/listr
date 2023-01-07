951019db249173bda174cc8924c20ede1d60edde
Boris <boris.grinshpun@hotmail.com>
2023-01-07 16:26:58 +0200

    css

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -5,37 +5,42 @@ import Button from '@mui/material/Button';
 
 import { useState, useEffect } from 'react'
 
-export default function typography({btnState, symbolClickHandler}) {
+export default function typography({ btnState, symbolClickHandler }) {
     const [comma, setComma] = useState(btnState.off)
     const [quote, setQuote] = useState(btnState.off)
     const [apostrophie, setApostrophie] = useState(btnState.off)
     const [spacing, setSpacing] = useState(btnState.off)
-    useEffect(()=>{
-        symbolClickHandler({quote: quote, apostrophie: apostrophie, comma: comma, spacing: spacing})
-    },[quote, apostrophie, comma, spacing])
-    function commaClickHandler(){
+    useEffect(() => {
+        symbolClickHandler({ quote: quote, apostrophie: apostrophie, comma: comma, spacing: spacing })
+    }, [quote, apostrophie, comma, spacing])
+    function commaClickHandler() {
         setComma(toggle(comma))
     }
-    function quoteClickHandler(){
+    function quoteClickHandler() {
         setApostrophie(btnState.off)
         setQuote(btnState.on)
     }
-    function apostrophieClickHandler(){
+    function apostrophieClickHandler() {
         setQuote(btnState.off)
         setApostrophie(btnState.on)
     }
-    function spacingClickHandler(){
+    function spacingClickHandler() {
         setSpacing(toggle(spacing))
     }
-    function toggle(state){
+    function toggle(state) {
         return state === btnState.on ? btnState.off : btnState.on
     }
     return (
         <div className="typography">
-            <Button variant={comma} title="Comma" onClick={commaClickHandler}><Comma className="icon" /></Button>
-            <Button variant={quote} title="Quote" onClick={quoteClickHandler}><Quote className="icon" /></Button>
-            <Button variant={apostrophie} title="Apostrophie" onClick={apostrophieClickHandler}><Apostrophie className="icon" /></Button>
-            <Button variant={spacing} onClick={spacingClickHandler} size="small" style={{ height: '2.5em' }}>spacing</Button>
+            <div className="sort-by">
+                <div className="label">
+                    Symbols
+                </div>
+                <Button variant={comma} title="Comma" onClick={commaClickHandler}><Comma className="icon" /></Button>
+                <Button variant={quote} title="Quote" onClick={quoteClickHandler}><Quote className="icon" /></Button>
+                <Button variant={apostrophie} title="Apostrophie" onClick={apostrophieClickHandler}><Apostrophie className="icon" /></Button>
+                <Button variant={spacing} onClick={spacingClickHandler} size="small" style={{ height: '2.5em' }}>spacing</Button>
+            </div>
         </div>
     )
 }
